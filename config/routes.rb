41a5c29e2bc3897fcb1b0704b95d4c59b9da1190
Karan Raj Baruah <karanrajbaruah@Karans-Air.desktops.cs.ait.ac.th>
2018-10-24 15:21:25 +0700

    Fixed view files

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get '/search' => 'ps2#search', :as => 'search_page'
   get '/ps2/:id/kill(.:format)' => 'ps2#kill', :as => 'killed'
   get 'ps2/del'
+  post 'ps2/import' => 'ps2#import'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
